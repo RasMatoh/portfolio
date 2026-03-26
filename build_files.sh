aa2ef0b48d4e4b161473ff2648b1a8167e4e4489
RasMatoh <kiunamartin2004@gmail.com>
2026-03-26 09:36:35 +0300

    fix: add --break-system-packages for Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Install dependencies first
-pip install -r requirements.txt
+# Install dependencies
+pip install -r requirements.txt --break-system-packages
  
 # Collect static files
 python manage.py collectstatic --noinput
